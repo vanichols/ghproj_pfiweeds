a537eba5141399057acb79f4311abc1bc8833ced
vanichols <vnichols@iastate.edu>
2020-03-27 15:46:46 -0500

    add bayes results as rd file in data folder

---
+++ code/stats-univariate-bayes-explore.R
@@ -0,0 +1,70 @@
+##################################
+# Author: Gina Nichols (vnichols@iastate.edu)
+# Created: march 27 2020
+#
+# Last modified: march 27 2020 
+#
+# Purpose: explore brms bayes results
+#
+# Outputs: 
+#
+# Notes: used model site_sys*cc_trt2 + (1|blockID) 
+#
+#
+####################################
+
+rm(list = ls())
+library(tidyverse)
+#devtools::install_github("vanichols/PFIweeds2020", force = TRUE) ## <-- run if pkg changes
+library(PFIweeds2020)
+library(tidybayes)
+library(janitor)
+
+
+# package data, for reference ---------------------------------------------
+
+dat <- pfifun_sum_byeu(pfi_ghobsraw) %>% 
+  unite(site_name, sys_trt, col = "site_sys", remove = F) 
+
+res_bayes <- read_csv("data/tidy/td_bayes1.csv")
+
+
+
+
+# try to visualize? -------------------------------------------------------
+
+res_bayes  %>%  
+  mutate(.variable = str_remove_all(.variable, "b_|cc_trt2|site_sys")) %>% 
+  pivot_wider(names_from = .variable, 
+              values_from = .value) %>% 
+  clean_names() %>% 
+  select(
+    chain, iteration, draw, 
+         intercept, #--intercept is actually boyd_grain_none 
+         ryecc, #--need to add to intercept to get boyd_grain_ryecc
+         boyd_silage_ryecc, 
+         funcke_grain_ryecc, 
+         stout_grain_ryecc
+    ) %>% 
+  mutate(
+    boyd_grain_ryecc = intercept - intercept + ryecc,
+         boyd_silage_ryecc = boyd_silage_ryecc,
+         funcke_ryecc = funcke_grain_ryecc,
+         stout_ryecc = stout_grain_ryecc
+    ) %>% 
+  select(chain, iteration, draw, boyd_grain_ryecc, boyd_silage_ryecc, funcke_ryecc, stout_ryecc) %>% 
+  pivot_longer(boyd_grain_ryecc:stout_ryecc) %>% 
+  ggplot(aes(x = value, y = reorder(name, -value, mean))) +
+  geom_vline(xintercept = 0, color = "gray50", size = 1.2, lty = 2, alpha = 0.5) +
+  geom_halfeyeh(fill = "gray80", .width = c(0.9)) +
+  #stat_pointintervalh(.width = c(.66, .95)) +
+  theme(legend.position = "none") +
+  labs(
+    y = NULL,
+    x = "Change in weed seeds, look at direction and relative magnitude")
+
+# no idea how to convert values into something meaningful. -0.8. 0.4. What the hell does that mean. 
+1/-.8
+-.8/1 # means it decreased 20%?
+
+
--- code/stats-univariate.R
+++ code/stats-univariate.R
@@ -9,6 +9,8 @@
 # Outputs: 
 #
 # Notes: m2 model (site_sys*cc_trt2 + (1|blockID) is the best fitting
+#     
+#        need to tidy emmeans output and write it
 #
 #
 ####################################
@@ -48,7 +50,7 @@ dstat <-
   dat %>% 
   mutate(cc_trt2 = recode(cc_trt,          ##--I want rye to appear first alphabetically?
                           no = "none",
-                          rye = "ryecc")) %>% 
+                          rye = "aryecc")) %>% 
   
   group_by(site_sys, sys_trt, blockID) %>% 
   mutate(ctl_mean_totseeds = mean(totseeds_m2),
@@ -65,7 +67,14 @@ emmeans(m1, pairwise ~ cc_trt2|site_sys, type = "response")
 m2 <- lmer(log(totseeds_m2) ~ site_sys * cc_trt2 + (1|blockID), data = filter(dstat, totseeds_m2 < 15000))
 anova(m2)
 summary(m2)
-emmeans(m2, pairwise ~ cc_trt2|site_sys, type = "response")
+
+# how the crap do I tidy emmeans output? I've done it before.....#####################################
+m2em <- (emmeans(m2, pairwise ~ cc_trt2|site_sys, type = "response"))
+marginal <- emmeans(oranges_rg1, "day")
+
+tidy(contrast(regrid(m2em), method = "pairwise"))
+
+
 #emmeans(m2, "cc_trt2")
 #emmeans(m2, pairwise ~ cc_trt2, type = "response")  #--back transformed to ratio
 #emmeans(m2, pairwise ~ cc_trt2, type = "lp") #--log scale? confused
@@ -156,48 +165,7 @@ res_bayes1 <- fit_bayes1 %>%
   tidybayes::gather_draws(`b_.*`, regex = TRUE) %>%
   ungroup()
 
-write_csv("data/
-
-
-library(janitor)
-dummy  %>%  
-  mutate(.variable = str_remove_all(.variable, "b_|cc_trt2|site_sys")) %>% 
-  pivot_wider(names_from = .variable, values_from = .value) %>% 
-  clean_names() %>% 
-  select(chain, iteration, draw, intercept, #--intercept is actually boyd_grain_rye 
-         ryecc, boyd_silage_ryecc, funcke_grain_ryecc, stout_grain_ryecc) %>% 
-  mutate(boyd_grain_ryecc = intercept - intercept + ryecc,
-         boyd_silage_ryecc = intercept + boyd_silage_ryecc,
-         funcke_ryecc = intercept + funcke_grain_ryecc,
-         stout_ryecc = intercept + stout_grain_ryecc) %>% 
-  select(chain, iteration, draw, boyd_grain_ryecc, boyd_silage_ryecc, funcke_ryecc, stout_ryecc) %>% 
-  pivot_longer(boyd_grain_ryecc:stout_ryecc)
-
-
-fit_bayes1 %>%
-  gather_draws(`b_.*`, regex = TRUE) %>%
-  ungroup() %>%
-  mutate(.variable = str_remove_all(.variable, "b_|cc_trt2|site_sys")) %>% 
-  pivot_wider(names_from = .variable, values_from = .value) %>% 
-  clean_names() %>% 
-  select(chain, iteration, draw, intercept, #--intercept is actually boyd_grain_rye 
-         ryecc, boyd_silage_ryecc, funcke_grain_ryecc, stout_grain_ryecc) %>% 
-  mutate(boyd_grain_ryecc = intercept - intercept + ryecc, #--I don't know what to do here....
-         funcke_ryecc = funcke_grain_ryecc,
-         stout_ryecc = stout_grain_ryecc) %>% 
-  select(chain, iteration, draw, boyd_grain_ryecc, boyd_silage_ryecc, funcke_ryecc, stout_ryecc) %>% 
-  pivot_longer(boyd_grain_ryecc:stout_ryecc) %>% 
-ggplot(aes(x = value, y = reorder(name, -value, mean))) +
-  geom_vline(xintercept = 0, color = "gray50", size = 1.2, lty = 2, alpha = 0.5) +
-  geom_halfeyeh(fill = "gray80", .width = c(0.9)) +
-  #stat_pointintervalh(.width = c(.66, .95)) +
-  #stat_pointintervalh(.width = 0.9, color = "red", alpha = 0.5) +
-  theme(legend.position = "none") +
-  labs(
-    y = NULL,
-    x = "Increase / decrease in weed seeds (log of total seeds)")
-
-
+res_bayes1 %>% write_rds("data/tidy/td_bayes1.rd")
 
 
 # try rstanarm ------------------------------------------------------------

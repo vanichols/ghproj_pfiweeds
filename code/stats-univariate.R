bb381a2afd2f1053848c72febb9088de7fa069aa
vanichols <vnichols@iastate.edu>
2020-03-26 19:13:16 -0500

    add rstanarm code, haven't run it yet

--- code/stats-univariate.R
+++ code/stats-univariate.R
@@ -15,10 +15,6 @@
 
 rm(list = ls())
 library(tidyverse)
-library(lme4) #--for mixed models
-library(lmerTest) #--to get significances
-library(broom)
-library(emmeans)
 #devtools::install_github("vanichols/PFIweeds2020", force = TRUE) ## <-- run if pkg changes
 library(PFIweeds2020)
 
@@ -38,14 +34,19 @@ dat %>%
             max = max(totseeds_m2),
             mean = mean(totseeds_m2))
 
-# matt doesn't use a ratio ------------------------------------------------
+# mixed model on data ------------------------------------------------
+
+library(lme4) #--for mixed models
+library(lmerTest) #--to get significances
+library(broom)
+library(emmeans)
 
 dstat <- 
   dat %>% 
   unite(site_name, sys_trt, col = "site_sys", remove = F) %>% 
   mutate(cc_trt2 = recode(cc_trt,          ##--I want rye to appear first alphabetically?
                           no = "none",
-                          rye = "aryecc")) %>% 
+                          rye = "ryecc")) %>% 
   mutate(repmatt = paste(site_name, sys_trt, rep))
 
 #--full data set
@@ -53,14 +54,13 @@ m1 <- lmer(log(totseeds_m2) ~ site_sys * cc_trt2 + (1|blockID), data = dstat)
 anova(m1)
 emmeans(m1, pairwise ~ cc_trt2|site_sys, type = "response")
 
-
 #--outlier removed
 m2 <- lmer(log(totseeds_m2) ~ site_sys * cc_trt2 + (1|blockID), data = filter(dstat, totseeds_m2 < 15000))
 anova(m2)
 emmeans(m2, pairwise ~ cc_trt2|site_sys, type = "response")
 emmeans(m2, "cc_trt2")
 emmeans(m2, pairwise ~ cc_trt2, type = "response")  #--back transformed to ratio
-emmeans(m2, pairwise ~ cc_trt2, type = "lp") #--log scale
+emmeans(m2, pairwise ~ cc_trt2, type = "lp") #--log scale? confused
 
 #--examples to help
 #pigs.emm.s <- emmeans(pigs.lm, "source")
@@ -68,39 +68,85 @@ emmeans(m2, pairwise ~ cc_trt2, type = "lp") #--log scale
 #emm_s.t <- emmeans(noise.lm, pairwise ~ size | type, )
 
 # maybe I should try bayesian? --------------------------------------------
-
+# https://juliasilge.com/blog/salary-gender/
 library(brms)
 options(mc.cores = parallel::detectCores())
 
-fit_bayes1 <- brm(
-  log(totseeds_m2) ~ (1 | blockID) + site_sys*cc_trt2,
-  data = filter(dstat, totseeds_m2 < 15000))
-
+# example from blog:
 # fit_bayes1 <- brm(
 #   ConvertedComp ~ (1 | DevType) + EdLevel + OpenSourcer +
 #     YearsCodePro + Dependents + Gender,
 #   data = modeling_df
 # )
 
+# note: it takes a few minutes
+fit_bayes1 <- brm(
+  log(totseeds_m2) ~ (1 | blockID) + site_sys*cc_trt2,
+  data = filter(dstat, totseeds_m2 < 15000))
+
+
 summary(fit_bayes1)
 
 library(tidybayes)
 
+dummy <- fit_bayes1 %>%
+  gather_draws(`b_.*`, regex = TRUE) %>%
+  ungroup() %>% 
+  group_by(.variable) %>%
+  slice(1:5) %>% 
+  ungroup()
+
+library(janitor)
+dummy  %>%  
+  mutate(.variable = str_remove_all(.variable, "b_|cc_trt2|site_sys")) %>% 
+  pivot_wider(names_from = .variable, values_from = .value) %>% 
+  clean_names() %>% 
+  select(chain, iteration, draw, intercept, #--intercept is actually boyd_grain_rye 
+         ryecc, boyd_silage_ryecc, funcke_grain_ryecc, stout_grain_ryecc) %>% 
+  mutate(boyd_grain_ryecc = intercept - intercept + ryecc,
+         boyd_silage_ryecc = intercept + boyd_silage_ryecc,
+         funcke_ryecc = intercept + funcke_grain_ryecc,
+         stout_ryecc = intercept + stout_grain_ryecc) %>% 
+  select(chain, iteration, draw, boyd_grain_ryecc, boyd_silage_ryecc, funcke_ryecc, stout_ryecc) %>% 
+  pivot_longer(boyd_grain_ryecc:stout_ryecc)
+
+
 fit_bayes1 %>%
   gather_draws(`b_.*`, regex = TRUE) %>%
   ungroup() %>%
-  mutate(
-    .variable = str_remove_all(.variable, "b_|cc_trt2|site_sys"),
-    .variable = str_replace_all(.variable, "none", "No Cover")
-  ) %>%
-  filter(.variable != "Intercept") %>%
-  ggplot(aes(x = .value, y = .variable)) +
+  mutate(.variable = str_remove_all(.variable, "b_|cc_trt2|site_sys")) %>% 
+  pivot_wider(names_from = .variable, values_from = .value) %>% 
+  clean_names() %>% 
+  select(chain, iteration, draw, intercept, #--intercept is actually boyd_grain_rye 
+         ryecc, boyd_silage_ryecc, funcke_grain_ryecc, stout_grain_ryecc) %>% 
+  mutate(boyd_grain_ryecc = intercept - intercept + ryecc,
+         funcke_ryecc = funcke_grain_ryecc,
+         stout_ryecc = stout_grain_ryecc) %>% 
+  select(chain, iteration, draw, boyd_grain_ryecc, boyd_silage_ryecc, funcke_ryecc, stout_ryecc) %>% 
+  pivot_longer(boyd_grain_ryecc:stout_ryecc) %>% 
+ggplot(aes(x = value, y = reorder(name, -value, mean))) +
   geom_vline(xintercept = 0, color = "gray50", size = 1.2, lty = 2, alpha = 0.5) +
-  geom_halfeyeh(fill = "gray80") +
-  stat_pointintervalh(.width = c(.66, .95)) +
+  geom_halfeyeh(fill = "gray80", .width = c(0.9)) +
+  #stat_pointintervalh(.width = c(.66, .95)) +
+  #stat_pointintervalh(.width = 0.9, color = "red", alpha = 0.5) +
   theme(legend.position = "none") +
   labs(
     y = NULL,
     x = "Increase / decrease in weed seeds (log of total seeds)")
 
 
+
+
+# try rstanarm ------------------------------------------------------------
+# https://mc-stan.org/users/documentation/case-studies/tutorial_rstanarm.html#using-the-rstanarm-package
+
+library(rstanarm)
+options(mc.cores = parallel::detectCores())
+
+# M1_stanlmer <- stan_lmer(formula = course ~ 1 + (1 | school), 
+#                          data = GCSE,
+#                          seed = 349)
+
+M1_stanlmer <- stan_lmer(formula = log(totseeds_m2) ~ (1 | blockID) + site_sys*cc_trt2, 
+                         data = filter(dstat, totseeds_m2 < 15000),
+                         seed = 349)

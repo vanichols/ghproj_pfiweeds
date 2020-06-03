fc0f73fe6ae7e353abe3764b2c917802584ab62b
vanichols <vnichols@iastate.edu>
2020-06-02 19:41:40 -0500

    start remaking table, quit I'm tired

--- 02_make-figs/code_table-changes.R
+++ 02_make-figs/code_table-changes.R
@@ -8,32 +8,50 @@
 #
 ####################################
 
+library(PFIweeds2020)
 library(readr)
 library(dplyr)
 library(ggplot2)
 library(tidyr)
 library(gt)
 
+
+# raw stat output ---------------------------------------------------------
+
+sb_pvals <- read_csv("01_stats-uni/st_weedseed-contr.csv")
+sb_est <- read_csv("01_stats-uni/st_weedseed-est.csv") 
+
+
+# convert it --------------------------------------------------------------
+#--this still isn't right.
+
+sb_est %>%
+  mutate(totseeds = exp(estimate),
+         totseeds_m2 = pfifun_seedstom2conv(totseeds)) %>% 
+  select(site_sys, cc_trt, totseeds_m2) %>% 
+  pivot_wider(names_from = cc_trt,
+              values_from = totseeds_m2) %>% 
+  mutate(Seeds = -round(no - rye, 0),
+         pct = paste0(round(Seeds/no * 100, 0), "%"))
+
+  
+  
+  #--rename sites
+  mutate(site_id = recode(site_sys,
+                          "Boyd_grain" = "Central1",
+                          "Boyd_silage" = "Central2 (Silage)",
+                          "Funcke_grain" = "West",
+                          "Stout_grain" = "East"),
+         site_id = factor(site_id, levels = c("West", "Central2 (Silage)", "Central1", "East"))) %>% 
+  mutate(Seeds = -round(no - rye, 0),
+         pct = paste0(round(Seeds/no * 100, 0), "%"))
+
 # table -------------------------------------------------------------------
 
-sb_pvals <- read_csv("01_stats-uni/st_weedseed-contrasts-simp.csv")
-  
-sb_est <- read_csv("01_stats-uni/st_weedseed-estimates-simp.csv") 
 
 dat_tbl <- 
   sb_est %>% 
-    select(site_sys, cc_trt, totseedsm2) %>% 
-    pivot_wider(names_from = cc_trt,
-                values_from = totseedsm2) %>% 
-    #--rename sites
-    mutate(site_id = recode(site_sys,
-                            "Boyd_grain" = "Central1",
-                            "Boyd_silage" = "Central2 (Silage)",
-                            "Funcke_grain" = "West",
-                            "Stout_grain" = "East"),
-           site_id = factor(site_id, levels = c("West", "Central2 (Silage)", "Central1", "East"))) %>% 
-    mutate(Seeds = -round(no - rye, 0),
-           pct = paste0(round(Seeds/no * 100, 0), "%")) %>% 
+    %>% 
     left_join(sb_pvals) %>% 
     mutate(p.value = round(p.value, 2)) %>% 
   mutate(est_pct = (1 - exp(estimate))*100)

8b50a2c4a1fba327090e6854b33586a7510f7018
vanichols <vnichols@iastate.edu>
2020-06-04 07:45:09 -0500

    making new figs

--- 02_make-figs/code-manu-bar-map-combo.R
+++ 02_make-figs/code-manu-bar-map-combo.R
@@ -81,9 +81,9 @@ fig_map <-
   geom_polygon(data = map_county3, aes(x = long, y = lat, group = group), 
                color = "gray80", fill = p_pink, size = 2) +
   
-  geom_text(aes(x = -95, y = 42.05), size = 5, label = "West") +
-  geom_text(aes(x = -93.3, y = 42.05), size = 5, label = "Central") +
-  geom_text(aes(x = -91.7, y = 41.60), size = 5, label = "East") +
+  geom_text(aes(x = -95.5, y = 42.05), size = 5, label = "West") +
+  geom_text(aes(x = -93., y = 42.05), size = 5, label = "Central") +
+  geom_text(aes(x = -91.7, y = 41.70), size = 5, label = "East") +
   theme_minimal()  +
   mylegendtheme +
   myaxistexttheme +
@@ -107,6 +107,28 @@ labseedsm2 = expression('Weed Seeds\n (1000s m'^"-2)")
 labseedsm2 <- bquote("Weed Seeds (1000s"~m^-2~")")
 
 #--raw values
+raws <-
+  pfi_ghobsraw %>% pfifun_sum_byeu() %>%
+  ungroup() %>%
+  rename(site = site_name,
+         crop_sys = sys_trt) %>%
+  mutate(
+    site = recode(
+      site,
+      "Boyd" = "Central",
+      "Funcke" = "West",
+      "Stout" = "East"
+    ),
+    site = factor(site, levels = c("West", "Central", "East")),
+    crop_sys = str_to_title(crop_sys),
+    cc_trt = recode(cc_trt,
+                    "no" = "None",
+                    "rye" = "Winter Rye")
+  ) %>% 
+  filter(totseeds_m2 < 15000)
+
+
+#--raw means 
 sb_est <- read_csv("01_stats-uni/st_weedseed-est.csv") %>% 
   mutate(totseeds_m2 = pfifun_seedstom2conv(rate),
          totseeds_se = pfifun_seedstom2conv(std.error),
@@ -175,7 +197,7 @@ fig_dat <-
     crop_sys = str_to_title(crop_sys),
     cc_trt = recode(cc_trt,
                     "no" = "None",
-                    "ccrye" = "Rye Cover Crop")) %>%
+                    "ccrye" = "Winter Rye")) %>%
     select(site, cc_trt, crop_sys, totseeds_m2, se_lo, se_hi)
 
 fig_dat %>% 
@@ -184,19 +206,23 @@ fig_dat %>%
            color = "black",
            size = 1.2,
            aes(fill = cc_trt)) +
+  geom_point(data = raws, aes(crop_sys, totseeds_m2/1000, color = cc_trt), 
+             pch = 21, position = position_dodge(0.9), size = 3, fill = "gray80", alpha = 0.5) +
+  
   geom_linerange(position = position_dodge(width = 0.9),
                  aes(ymin = se_lo / 1000, ymax = se_hi / 1000, alpha = cc_trt)) +
-  geom_text(data = table_changes, aes(x = crop_sys, y = se_mx + 0.5, label = p.value), fontface = "italic") +
-  geom_text(data = table_changes, aes(x = crop_sys, y = se_mx + 1, label = trt_eff), fontface = "italic") +
-  geom_text(data = table_changes, aes(x = crop_sys, y = se_mx + 1.5, label = trt_eff_pct2), fontface = "italic") +
+  geom_text(data = table_changes, aes(x = crop_sys, y = se_mx + 1.5, label = p.value), fontface = "italic") +
+  geom_text(data = table_changes, aes(x = crop_sys, y = se_mx + 2, label = trt_eff), fontface = "italic") +
+  geom_text(data = table_changes, aes(x = crop_sys, y = se_mx + 2.5, label = trt_eff_pct2), fontface = "italic") +
   scale_alpha_manual(values = c(1, 1)) +
   labs(y = labseedsm2,
        x = NULL,
-       fill = NULL) +
-  guides(alpha = F) +
-  scale_fill_manual(values = c("None" = cctrtpal[2],
-                               "Rye Cover Crop" = cctrtpal[1])) +
-  scale_color_manual(values = cctrtpal) +
+       fill = "Cover Crop Treatment") +
+  guides(alpha = F,
+         color = F) +
+  scale_fill_manual(values = c("None" = p_yellow,
+                               "Winter Rye" = p_blue)) +
+  scale_color_manual(values = c("gray50", "gray50")) +
   theme_bw() +
   facet_grid(. ~ site, scales = "free") +
   myaxistexttheme +
@@ -205,6 +231,7 @@ fig_dat %>%
         legend.justification = c(1, 1),
         legend.position = c(0.99, 0.99),
         legend.background = element_rect(color = "black", fill = "white"),
+        legend.title = element_text(size = rel(1.4)),
         #legend.key.width = unit(1.4, "cm"),
         #legend.key.height = unit(0.5, "cm"),
         # legend.key.size = unit(1, "cm"),
@@ -218,17 +245,20 @@ ggsave("02_make-figs/figs/fig1_bar-totseeds.png")
 
 # put together ------------------------------------------------------------
 
+
+fig_map/fig_sb +  plot_layout(heights = c(1, 2))
+
 #--total hack, doesn't work on desktop, works on laptop
-#library(gridExtra)
-#fig_sb + (fig_map /gridExtra::tableGrob(mtcars[1:10, c('mpg', 'disp')]))
+library(gridExtra)
+fig_sb + (fig_map /gridExtra::tableGrob(mtcars[1:10, c('mpg', 'disp')]))
 
-layout <- c(
-  area(t = 2, l = 1, b = 5, r = 4),
-  area(t = 1, l = 3, b = 3, r = 5)
-)
-
-fig_sb + fig_map + 
-  plot_layout(design = layout)
+# layout <- c(
+#   area(t = 2, l = 1, b = 5, r = 4),
+#   area(t = 1, l = 3, b = 3, r = 5)
+# )
+# 
+# fig_sb + fig_map + 
+#   plot_layout(design = layout)
 
 
 ggsave("02_make-figs/figs/fig1_bar-map.png")
